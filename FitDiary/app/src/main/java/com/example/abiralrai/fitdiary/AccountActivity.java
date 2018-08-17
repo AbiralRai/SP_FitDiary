75b75532443d38cd67b9c232a9b584b724d6381b
Abiralrai <abiralrai182@gmail.com>
2018-08-17 18:00:10 +1000

    First commit

---
+++ FitDiary/app/src/main/java/com/example/abiralrai/fitdiary/AccountActivity.java
@@ -0,0 +1,45 @@
+package com.example.abiralrai.fitdiary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitError;
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+
+import java.util.Locale;
+
+public class AccountActivity extends AppCompatActivity {
+
+    TextView id;
+    TextView infoLabel;
+    TextView info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+
+    }
+
+    public void onLogout(View view) {
+        // logout of Account Kit
+        AccountKit.logOut();
+        launchLoginActivity();
+    }
+
+    private void launchLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
